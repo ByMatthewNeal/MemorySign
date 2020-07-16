47c04a01be36eadac5a65b6a2ad6dffda50dcc32
Matthew Neal <mneal.jw@gmail.com>
2020-07-16 18:30:30 -0400

    just scrapped my html to make it more condensed and readable

--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+
+// User will flip card
 function flipCard() {
     this.classList.toggle('flip');
+   
+    if(!hasFlippedCard) {
+        // First Click
+    hasFlipped = true;
+    firstCard = this;
+       // Second Click
+   }   else {
+    hasFlippedCard = false;
+    secondCard = this;
+
+    //Cards Match?
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard))
+
+    console.log({hasFlippedCard, firstCard});
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
